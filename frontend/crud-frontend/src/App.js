8fb86e7340342dbd1c58fc2c4f2cf344c7b1fc0f
Camis <cvn.camila@gmail.com>
2020-11-02 21:51:55 -0300

    criação do frontend;

--- frontend/crud-frontend/src/App.js
+++ frontend/crud-frontend/src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 function App() {
+  const baseUrl = "https://localhost:44351/api/gestores";
+  const [data, setData]=useState([])
+
+  const gestoresGet=async()=>{
+    await axios.get(baseUrl)
+    .then(response=>{
+      setData(response.data);
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  useEffect(()=>{
+    getSelection();
+  },[])
+
   return (
     <div className="App">
-      
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Código</th>  
+            <th>Nome</th>  
+            <th>Lançamento</th>  
+            <th>Desenvolvedor</th>  
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>  
     </div>
   );
 }
